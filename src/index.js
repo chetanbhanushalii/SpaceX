06ae2fe122035ea76f7d91d959782a12d06ccc84
Chetan Bhanushali <chetandimmak@gmail.com>
2019-11-12 18:15:24 +0530

    checking

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import Launchpads from './launchpad';
 import Mission from './mission';
 import Payload from './payload';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 const  routing = (
     <Router>
@@ -25,4 +25,4 @@ ReactDOM.render(routing ,document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+serviceWorker.unregister();
