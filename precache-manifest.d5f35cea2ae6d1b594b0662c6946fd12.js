3ea6215fc2e53bb9e006e648850b840f492bc2d2
Chetan Bhanushali <chetandimmak@gmail.com>
2019-11-12 12:49:47 +0530

    Updates

---
+++ precache-manifest.d5f35cea2ae6d1b594b0662c6946fd12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a420a753c331b02e5fc8c2bbd95c29",
+    "url": "./index.html"
+  },
+  {
+    "revision": "06b72f93d07e9ddaa717",
+    "url": "./static/css/main.e3217357.chunk.css"
+  },
+  {
+    "revision": "88394a83a3d86599d218",
+    "url": "./static/js/2.f03bf837.chunk.js"
+  },
+  {
+    "revision": "06b72f93d07e9ddaa717",
+    "url": "./static/js/main.37a9ed52.chunk.js"
+  },
+  {
+    "revision": "8fd295c6c5655346d954",
+    "url": "./static/js/runtime-main.75f7af5e.js"
+  }
+]);
