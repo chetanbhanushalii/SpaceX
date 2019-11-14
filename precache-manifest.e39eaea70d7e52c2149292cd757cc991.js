a3119f5d08892d98472f0124cc5fc0a4ee17d55a
Chetan Bhanushali <chetandimmak@gmail.com>
2019-11-14 11:48:41 +0530

    Updates

--- precache-manifest.e39eaea70d7e52c2149292cd757cc991.js
+++ precache-manifest.e39eaea70d7e52c2149292cd757cc991.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "58e48d8272930093b5e860ed26f72832",
+    "revision": "309203e6cd6cb69f22d4a119c4cf2911",
     "url": "/SpaceX/index.html"
   },
   {
-    "revision": "f0e92e5fa722942b2759",
+    "revision": "6bc88054586f2bdd2a9b",
     "url": "/SpaceX/static/css/main.e3217357.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/SpaceX/static/js/2.625bc9d3.chunk.js"
   },
   {
-    "revision": "f0e92e5fa722942b2759",
-    "url": "/SpaceX/static/js/main.acbf4f37.chunk.js"
+    "revision": "6bc88054586f2bdd2a9b",
+    "url": "/SpaceX/static/js/main.d7ca336c.chunk.js"
   },
   {
     "revision": "342b524ba0d08be3e4c3",
